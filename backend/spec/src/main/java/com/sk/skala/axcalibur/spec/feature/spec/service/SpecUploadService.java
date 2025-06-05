5b7651358c353c1ff36f8e1615212fbd3b5d62fb
miiinnn23 <ansnnd123@naver.com>
2025-06-05 19:33:22 +0900

    feat : Service 전체 구현 추가

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecUploadService.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecUploadService.java
@@ -1,11 +1,16 @@
 package com.sk.skala.axcalibur.spec.feature.spec.service;
 
+import org.springframework.stereotype.Service;
+
+import com.sk.skala.axcalibur.spec.feature.spec.dto.request.SpecUploadRequest;
+import com.sk.skala.axcalibur.spec.feature.spec.dto.response.SpecUploadResponse;
+
 /**
  * 명세서 업로드 로직 구현
- * - 파일 유효성 검사(추후)
- * - 명세서 파일명/확장자만 단순 파싱
- * - 파싱된 정보 기반으로 
+ * - PVC 저장 파트에 넘겨줌 -> 이후 DB까지 저장된 파일 정보 받아 응답으로 반환
  */
+@Service
 public interface SpecUploadService {
-    
+    // Controller에서 받아온 project ID와 request body 받아오는 함수
+    public SpecUploadResponse uploadFiles(String projectIdStr, SpecUploadRequest request);
 }
