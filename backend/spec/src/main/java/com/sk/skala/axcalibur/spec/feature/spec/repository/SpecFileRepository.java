9d42f80f463f7dd3ce6ae8ab6e73fe9befbae9ca
yujeong1122 <jjjjj0407@gmail.com>
2025-06-09 20:26:28 +0900

    feat: SpecFileRepository에 프로젝트 기준 삭제 메서드 추가

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/repository/SpecFileRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/repository/SpecFileRepository.java
@@ -1,6 +1,10 @@
 package com.sk.skala.axcalibur.spec.feature.spec.repository;
 
+import com.sk.skala.axcalibur.spec.feature.spec.entity.ProjectEntity;
 import com.sk.skala.axcalibur.spec.feature.spec.entity.SpecFileEntity;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +12,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpecFileRepository extends JpaRepository<SpecFileEntity, Integer> {
-
+    @Transactional
+    void deleteAllByProject(ProjectEntity project);
 }
