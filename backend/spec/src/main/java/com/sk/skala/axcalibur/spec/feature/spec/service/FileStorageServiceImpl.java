329376c79910c6fa5af5ea3013fd66460c94c743
yujeong1122 <jjjjj0407@gmail.com>
2025-06-09 15:48:29 +0900

    feat: application.yml에 file.basepath 설정 추가

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/FileStorageServiceImpl.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/FileStorageServiceImpl.java
@@ -3,13 +3,17 @@ package com.sk.skala.axcalibur.spec.feature.spec.service;
 import java.io.File;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import lombok.val;
 
 @Service
 public class FileStorageServiceImpl implements FileStorageService {
 
-    private static final String basepath = "uploads/"; // 추후 pvc 경로로 변경 필요
+    @Value("${file.basepath}")
+    private String basepath; // 추후 pvc 경로로 변경 필요
 
     @Override
     public String storeFile(MultipartFile file, String projectId) {
