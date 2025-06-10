d65cbcef0f047ca0a2f448a3d5d4f9d734c79195
miiinnn23 <ansnnd123@naver.com>
2025-06-10 09:27:19 +0900

    refactor: 쿠키 전달 방식 수정

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/controller/SpecUploadController.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/controller/SpecUploadController.java
@@ -1,10 +1,9 @@
 package com.sk.skala.axcalibur.spec.feature.spec.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 import com.sk.skala.axcalibur.spec.feature.spec.dto.response.SpecUploadResponse;
 import com.sk.skala.axcalibur.spec.global.response.SuccessResponse;
@@ -14,7 +13,7 @@ import io.swagger.v3.oas.annotations.Operation;
 /**
  * 명세서 업로드 컨트롤러 인터페이스
  * '명세서 업로드(IF-SP-0001)' 파트를 구현합니다.
- * @param request HttpServletRequest 객체
+ * @param key 인증용 Cookie 문자열
  * @param requirementFile 요구사항 정의서 파일
  * @param interfaceDef 인터페이스 정의서 파일
  * @param interfaceDesign 인터페이스 설계서 파일
@@ -24,7 +23,7 @@ public interface SpecUploadController {
 
     @Operation(summary = "명세서 업로드", description = "요구사항정의서, 인터페이스정의서, 설계서를 업로드합니다.")
     public ResponseEntity<SuccessResponse<SpecUploadResponse>> uploadSpec(
-        HttpServletRequest request,
+        @CookieValue("avalon") String key,
         @RequestParam MultipartFile requirementFile,
         @RequestParam MultipartFile interfaceDef,
         @RequestParam MultipartFile interfaceDesign
--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/ProjectIdResolverService.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/ProjectIdResolverService.java
@@ -1,12 +1,9 @@
 package com.sk.skala.axcalibur.spec.feature.spec.service;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 /**
  * Project ID를 조회하는 로직 구현
- * Redis에서 인증값으로 project ID를 조회합니다.
- * 쿠키에서 project key(avalon)를 추출합니다.
+ * Redis에서 쿠키의 key를 기반으로 실제 project id를 추출합니다.
  */
 public interface ProjectIdResolverService {
-    public String resolveProjectId(HttpServletRequest request);
+    public String resolveProjectId(String key);
 }
