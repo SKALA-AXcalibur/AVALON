db7fc5428b8b5b64ca7904e50f0f189a570514d6
miiinnn23 <ansnnd123@naver.com>
2025-06-10 15:59:55 +0900

    refactor: 응답 구조 변환(response dto 삭제 -> 성공 여부만 반환)

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecUploadService.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecUploadService.java
@@ -3,14 +3,13 @@ package com.sk.skala.axcalibur.spec.feature.spec.service;
 import org.springframework.stereotype.Service;
 
 import com.sk.skala.axcalibur.spec.feature.spec.dto.request.SpecUploadRequest;
-import com.sk.skala.axcalibur.spec.feature.spec.dto.response.SpecUploadResponse;
 
 /**
  * 명세서 업로드 로직 구현
- * - PVC 저장 파트에 넘겨줌 -> 이후 DB까지 저장된 파일 정보 받아 응답으로 반환
+ * - PVC 저장 파트에 넘겨줌 -> 이후 DB까지 저장 결과 받아 응답으로 반환
  */
 @Service
 public interface SpecUploadService {
     // Controller에서 받아온 project ID와 request body 받아오는 함수
-    public SpecUploadResponse uploadFiles(String projectIdStr, SpecUploadRequest request);
+    public void uploadFiles(String projectIdStr, SpecUploadRequest request);
 }
