c7617ad8d002ed9c89f60531c438d27c78ce2c24
kimseongyu <kskksk0925@naver.com>
2025-06-18 21:19:33 +0900

    feat: scenario ui 개선

--- frontend/src/components/common/ActionButton.tsx
+++ frontend/src/components/common/ActionButton.tsx
@@ -1,16 +1,19 @@
 const ActionButton = ({
   onClick,
   color,
+  disabled,
   children,
 }: {
   onClick: () => void;
   color: string;
+  disabled?: boolean;
   children: React.ReactNode;
 }) => {
   return (
     <button
       onClick={onClick}
-      className={`${color} text-white rounded-lg px-4 py-2 flex items-center justify-center gap-1`}
+      className={`${color} text-white rounded-lg px-4 py-2 flex items-center justify-center gap-1 cursor-pointer`}
+      disabled={disabled}
     >
       {children}
     </button>
