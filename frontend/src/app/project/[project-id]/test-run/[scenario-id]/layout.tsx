89e2f5f35fcb945d4863c7efd881504147d5e85b
kimseongyu <kskksk0925@naver.com>
2025-06-19 22:56:27 +0900

    feat: api test 창 기능 개선

---
+++ frontend/src/app/project/[project-id]/test-run/[scenario-id]/layout.tsx
@@ -0,0 +1,25 @@
+import { TestRunSidebar } from "@/components/test-run/TestRunSidebar";
+
+const TestRunLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {
+    "project-id": string;
+    "scenario-id": string;
+  };
+}) => {
+  const { "project-id": projectId, "scenario-id": scenarioId } = await params;
+
+  return (
+    <div className="flex flex-1">
+      <TestRunSidebar projectId={projectId} scenarioId={scenarioId} />
+      <main className="flex-1 p-12 overflow-y-auto h-[calc(100vh-84px)]">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default TestRunLayout;
