a80040264bc3d6bf9e9bb690f4fd8442e08b97cc
kimseongyu <kskksk0925@naver.com>
2025-06-19 10:19:57 +0900

    refactor: testcase api 경로 수정

--- frontend/src/services/client/clientTestcaseApi.ts
+++ frontend/src/services/client/clientTestcaseApi.ts
@@ -14,7 +14,7 @@ export const clientTestcaseApi = {
     scenarioId: string,
     testcase: createTestcaseRequest
   ): Promise<createTestcaseResponse> => {
-    const response = await ky.post(`${BASE_URL}/${scenarioId}`, {
+    const response = await ky.post(`${BASE_URL}/scenario/${scenarioId}`, {
       credentials: "include",
       json: testcase,
     });
