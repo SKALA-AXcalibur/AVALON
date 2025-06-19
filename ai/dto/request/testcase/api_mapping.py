ff12849936d74acc9dd800d2d6df30bc0d35ce32
miiinnn23 <ansnnd123@naver.com>
2025-06-19 17:42:35 +0900

    refactor: DTO 파라미터 명 수정

--- ai/dto/request/testcase/api_mapping.py
+++ ai/dto/request/testcase/api_mapping.py
@@ -1,5 +1,5 @@
 # ai/dto/request/testcase/api_mapping.py
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 from dto.request.testcase.param import Param
@@ -9,11 +9,14 @@ API 매핑 객체 정의
 (매핑표 ID, 단계, API 이름, URL, Path, HTTP Method, 설명, 파라미터 목록)
 """
 class ApiMapping(BaseModel):
-    mapping_id: int
+    mapping_id: int = Field(..., alias="mappingId")
     step: int
-    name: str
+    name: Optional[str] = None
     url: Optional[str] = None
     path: str
     method: str
     desc: str
-    param_list: List[Param]
+    param_list: List[Param] = Field(..., alias="paramList")
+
+    class Config:
+        validate_by_name = True
--- ai/dto/request/testcase/scenario.py
+++ ai/dto/request/testcase/scenario.py
@@ -1,5 +1,5 @@
 # ai/dto/request/testcase/scenario.py
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 """
@@ -7,6 +7,9 @@ from typing import Optional
 (시나리오 이름, 시나리오 설명, 검증포인트)
 """
 class Scenario(BaseModel):
-    scenario_name: str
-    scenario_desc: str
+    scenario_name: str = Field(..., alias="scenarioName")
+    scenario_desc: str = Field(..., alias="scenarioDesc")
     validation: Optional[str] = None
+    
+    class Config:
+        validate_by_name = True
