b727c60aa6e6bb4ed7cbb5cbd7b875c232e0ca4d
miiinnn23 <ansnnd123@naver.com>
2025-06-19 17:38:55 +0900

    refactor: DB 설계서 관련 내용 삭제

--- ai/dto/request/testcase/tc_generation_request.py
+++ ai/dto/request/testcase/tc_generation_request.py
@@ -1,16 +1,17 @@
 # ai/dto/request/testcase/tc_generation_request.py
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 from dto.request.testcase.api_mapping import ApiMapping
 from dto.request.testcase.scenario import Scenario
-from dto.request.testcase.table import Table
 
 """
 TC생성 Request DTO
-(시나리오, API 매핑표, 테이블 설계 정보)
+(시나리오, API 매핑표)
 """
 class TestcaseGenerationRequest(BaseModel):
     scenario: Scenario
-    api_mapping_list: List[ApiMapping]
-    db_list: List[Table]
+    api_mapping_list: List[ApiMapping] = Field(..., alias="apiMappingList")
+
+    class Config:
+        validate_by_name = True
