494004803cacd51dce6f024e7cbc7277d2928882
yujeong1122 <jjjjj0407@gmail.com>
2025-06-19 14:33:18 +0900

    feat: FileTypeName enum 추가 및 경로 조회 메서드의 매개변수 타입 변경

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecAnalyzeService.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/service/SpecAnalyzeService.java
@@ -2,9 +2,10 @@ package com.sk.skala.axcalibur.spec.feature.spec.service;
 
 import java.util.Map;
 
+import com.sk.skala.axcalibur.spec.feature.spec.enums.FileTypeName;
 import com.sk.skala.axcalibur.spec.global.entity.ProjectEntity;
 
 public interface SpecAnalyzeService {
     Map<String, String> analyze(ProjectEntity project);
-    String getPath(ProjectEntity project, String typeName);
+    String getPath(ProjectEntity project, FileTypeName typeName);
 }
