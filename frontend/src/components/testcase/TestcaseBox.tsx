1b2f33148954a074794a312e88f79f6bd65d4fef
kimseongyu <kskksk0925@naver.com>
2025-07-01 16:28:03 +0900

    feat: 테스트케이스 창 네비게이션 개선

--- frontend/src/components/testcase/TestcaseBox.tsx
+++ frontend/src/components/testcase/TestcaseBox.tsx
@@ -74,27 +74,6 @@ export const TestcaseBox = ({
               )}
             </div>
             <div className="flex gap-2">
-              <LinkButton
-                href={`/project/${projectId}/scenario/new`}
-                color="bg-transparent text-slate-700 hover:text-orange-500"
-                ariaLabel="시나리오 추가"
-              >
-                시나리오 추가
-              </LinkButton>
-              <LinkButton
-                href={`/project/${projectId}/scenario/${scenarioId}/testcase/new`}
-                color="bg-transparent text-slate-700 hover:text-orange-500"
-                ariaLabel="TC 추가"
-              >
-                TC 추가
-              </LinkButton>
-              <ActionButton
-                onClick={() => handleDelete(onDeleteSuccess)}
-                color="bg-transparent text-slate-700 hover:text-orange-500"
-                disabled={isLoading}
-              >
-                삭제
-              </ActionButton>
               {testcaseId === "new" ? (
                 <ActionButton
                   onClick={() => handleCreate(onCreateSuccess)}
@@ -104,13 +83,36 @@ export const TestcaseBox = ({
                   생성
                 </ActionButton>
               ) : (
-                <ActionButton
-                  onClick={handleUpdate}
-                  color="bg-transparent text-slate-700 hover:text-orange-500"
-                  disabled={isLoading}
-                >
-                  저장
-                </ActionButton>
+                <>
+                  <LinkButton
+                    href={`/project/${projectId}/scenario/new`}
+                    color="bg-transparent text-slate-700 hover:text-orange-500"
+                    ariaLabel="시나리오 추가"
+                  >
+                    시나리오 추가
+                  </LinkButton>
+                  <LinkButton
+                    href={`/project/${projectId}/scenario/${scenarioId}/testcase/new`}
+                    color="bg-transparent text-slate-700 hover:text-orange-500"
+                    ariaLabel="TC 추가"
+                  >
+                    TC 추가
+                  </LinkButton>
+                  <ActionButton
+                    onClick={() => handleDelete(onDeleteSuccess)}
+                    color="bg-transparent text-slate-700 hover:text-orange-500"
+                    disabled={isLoading}
+                  >
+                    삭제
+                  </ActionButton>
+                  <ActionButton
+                    onClick={handleUpdate}
+                    color="bg-transparent text-slate-700 hover:text-orange-500"
+                    disabled={isLoading}
+                  >
+                    저장
+                  </ActionButton>
+                </>
               )}
             </div>
           </div>
