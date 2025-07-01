e70bad3091a5ca88e383d431d9b66fc45d934482
yujeong1122 <jjjjj0407@gmail.com>
2025-07-01 22:02:32 +0900

    refactor: 기존 엔티티, repository 삭제

--- backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/testcase/repository/MappingRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.sk.skala.axcalibur.feature.testcase.repository;
-
-import java.util.List;
-
-import com.sk.skala.axcalibur.feature.testcase.dto.response.ApiListDto;
-
-public interface MappingRepositoryCustom {
-    List<ApiListDto> findApiListByScenarioId(String scenarioId);
-}
--- backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/testcase/repository/TestCaseDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sk.skala.axcalibur.feature.testcase.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sk.skala.axcalibur.feature.testcase.entity.TestCaseDataEntity;
-
-public interface TestCaseDataRepository extends JpaRepository<TestCaseDataEntity, Integer>, TestCaseDataRepositoryCustom {
-}
