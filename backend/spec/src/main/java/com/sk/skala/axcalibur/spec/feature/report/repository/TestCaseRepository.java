6cf301298559e8bbca6ed53fbdf5631344d1cd1f
ard1019 <rio1019@naver.com>
2025-07-03 15:23:34 +0900

    feat: Repository 쿼리 메서드 추가 및 @Repository 어노테이션 적용

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseRepository.java
@@ -1,9 +1,14 @@
 package com.sk.skala.axcalibur.spec.feature.report.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sk.skala.axcalibur.spec.feature.report.entity.MappingEntity;
 import com.sk.skala.axcalibur.spec.feature.report.entity.TestCaseEntity;
 
+@Repository
 public interface TestCaseRepository extends JpaRepository<TestCaseEntity, Integer> {
-    
+    List<TestCaseEntity> findByMappingKeyIn(List<MappingEntity> mappingEntities);
 }
