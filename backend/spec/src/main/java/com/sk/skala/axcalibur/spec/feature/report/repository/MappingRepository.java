da9642dfc6fa01a5d52b06f751c602a8e1d7d4e2
ard1019 <rio1019@naver.com>
2025-07-03 15:23:34 +0900

    feat: Repository 쿼리 메서드 추가 및 @Repository 어노테이션 적용

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/MappingRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/MappingRepository.java
@@ -1,9 +1,13 @@
 package com.sk.skala.axcalibur.spec.feature.report.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sk.skala.axcalibur.spec.feature.report.entity.MappingEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MappingRepository extends JpaRepository<MappingEntity, Integer> {
-    
+    List<MappingEntity> findByScenarioKey_ScenarioId(String scenarioId);
 }
--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseDataRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseDataRepository.java
@@ -1,9 +1,14 @@
 package com.sk.skala.axcalibur.spec.feature.report.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sk.skala.axcalibur.spec.feature.report.entity.TestCaseDataEntity;
+import com.sk.skala.axcalibur.spec.feature.report.entity.TestCaseEntity;
 
+@Repository
 public interface TestCaseDataRepository extends JpaRepository<TestCaseDataEntity, Integer> {
-    
+    List<TestCaseDataEntity> findByTestcaseKeyIn(List<TestCaseEntity> testCaseEntities);
 }
