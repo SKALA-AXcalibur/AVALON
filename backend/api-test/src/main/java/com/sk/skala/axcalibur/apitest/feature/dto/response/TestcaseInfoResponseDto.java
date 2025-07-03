417cf142da68799048d8af1533e2841424e5417a
dig04214 <dig04214@naver.com>
2025-07-03 11:44:50 +0900

    fix : 오타 수정
    - IF-AT-0003 응답 오타 수정

--- backend/api-test/src/main/java/com/sk/skala/axcalibur/apitest/feature/dto/response/TestcaseInfoResponseDto.java
+++ backend/api-test/src/main/java/com/sk/skala/axcalibur/apitest/feature/dto/response/TestcaseInfoResponseDto.java
@@ -7,21 +7,10 @@ import lombok.Builder;
 
 @Builder
 public record TestcaseInfoResponseDto(
-    @NotBlank
-    @Schema(description = "테스트케이스 ID")
-    String tcId,
-    @NotBlank
-    @Schema(description = "테스트케이스 설명")
-    String description,
-    @NotBlank
-    @Schema(description = "테스트케이스 예상 결과")
-    String expectedResult,
-    @NotBlank
-    @Schema(description = "테스트케이스 성공 여무")
-    String isSuccess,
-    @Nullable
-    @Schema(description = "테스트케이스 수행 시간")
-    Double excutedTime
-) {
+    @NotBlank @Schema(description = "테스트케이스 ID") String tcId,
+    @NotBlank @Schema(description = "테스트케이스 설명") String description,
+    @NotBlank @Schema(description = "테스트케이스 예상 결과") String expectedResult,
+    @NotBlank @Schema(description = "테스트케이스 성공 여부") String isSuccess,
+    @Nullable @Schema(description = "테스트케이스 수행 시간") Double executedTime) {
 
 }
