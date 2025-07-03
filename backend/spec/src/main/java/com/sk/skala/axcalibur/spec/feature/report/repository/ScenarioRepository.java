c752e18d257c346cbf435b8c448b0aa9c155d80f
ard1019 <rio1019@naver.com>
2025-07-03 15:23:34 +0900

    feat: Repository 쿼리 메서드 추가 및 @Repository 어노테이션 적용

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/ScenarioRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/ScenarioRepository.java
@@ -1,11 +1,15 @@
 package com.sk.skala.axcalibur.spec.feature.report.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.sk.skala.axcalibur.spec.feature.report.entity.ScenarioEntity;
 
+@Repository
 public interface ScenarioRepository extends JpaRepository<ScenarioEntity, Integer> {
     Optional<ScenarioEntity> findByScenarioId(String scenarioId);
+
+    List<ScenarioEntity> findByProject_Id(Integer projectKey);
 }
