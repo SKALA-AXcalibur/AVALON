0615f4223fe10770896be95c09b1dcaff51ad33d
ard1019 <rio1019@naver.com>
2025-07-04 06:38:28 +0900

    테스트케이스 결과 엔티티, 레포지토리 추가

---
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestcaseResultRepository.java
@@ -0,0 +1,11 @@
+package com.sk.skala.axcalibur.spec.feature.report.repository;
+
+import com.sk.skala.axcalibur.spec.feature.report.entity.TestcaseResultEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestcaseResultRepository extends JpaRepository<TestcaseResultEntity, Integer>{
+  List<TestcaseResultEntity> findAllByTestcaseId(Integer testcaseKey);
+}
