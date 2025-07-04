a379090d5aa154ac392d0f32f198ed1c2e1c1c50
ard1019 <rio1019@naver.com>
2025-07-04 15:17:24 +0900

    다중 쿼리 발생 문제 해결

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/TestCaseRepository.java
@@ -3,6 +3,8 @@ package com.sk.skala.axcalibur.spec.feature.report.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sk.skala.axcalibur.spec.feature.report.entity.MappingEntity;
@@ -14,4 +16,14 @@ import com.sk.skala.axcalibur.spec.feature.report.entity.TestCaseEntity;
 @Repository
 public interface TestCaseRepository extends JpaRepository<TestCaseEntity, Integer> {
     List<TestCaseEntity> findByMappingKeyIn(List<MappingEntity> mappingEntities);
+
+    @Query("SELECT maj.name, COUNT(tc) FROM TestCaseEntity tc " +
+           "JOIN tc.mappingKey m " +
+           "JOIN m.apiListKey a " +
+           "JOIN a.requestKey r " +
+           "JOIN r.majorKey maj " +
+           "WHERE tc.mappingKey IN :mappings " +
+           "GROUP BY maj.name " +
+           "ORDER BY COUNT(tc) DESC")
+    List<Object[]> findMostUsedBusinessFunction(@Param("mappings") List<MappingEntity> mappings);
 }
