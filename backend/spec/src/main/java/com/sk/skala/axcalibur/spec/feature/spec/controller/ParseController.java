e409bbeea0009be653db75fe64af1329fadf16ea
miiinnn23 <ansnnd123@naver.com>
2025-07-04 14:20:08 +0900

    fix: 반환타입 그냥 void로 결정

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/controller/ParseController.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/spec/controller/ParseController.java
@@ -3,7 +3,6 @@ package com.sk.skala.axcalibur.spec.feature.spec.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 
-import com.sk.skala.axcalibur.spec.feature.spec.dto.EmptyResponseDto;
 import com.sk.skala.axcalibur.spec.global.response.SuccessResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,7 +18,7 @@ import io.swagger.v3.oas.annotations.Operation;
 public interface ParseController {
 
     @Operation(summary = "명세서 분석 요청", description = "명세서 분석을 시작합니다.")
-    ResponseEntity<SuccessResponse<EmptyResponseDto>> analyzeSpec(
+    ResponseEntity<SuccessResponse<Void>> analyzeSpec(
         @CookieValue("avalon") String key
 
     );
