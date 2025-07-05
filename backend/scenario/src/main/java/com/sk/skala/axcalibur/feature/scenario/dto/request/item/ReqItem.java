9dbe65836ddb12408f4f2ed5cec884d969d6e8fd
yujeong1122 <jjjjj0407@gmail.com>
2025-07-05 12:59:41 +0900

    feat: ScenarioGenRequestDto 및 ScenarioResponseDto에 JSON 속성 추가, ApiItem 및 ReqItem에서 불필요한 필드 제거

--- backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/scenario/dto/request/item/ReqItem.java
+++ backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/scenario/dto/request/item/ReqItem.java
@@ -11,10 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReqItem {
 
-    private String reqId;       // 요구사항 아이디
     private String name;        // 요구사항 이름
     private String desc;        // 요구사항 설명  
-    private String priority;    // 요구사항 우선도
     private String major;       // 요구사항 대분류
     private String middle;      // 요구사항 중분류
     private String minor;       // 요구사항 소분류
--- backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/scenario/dto/response/item/ApiItem.java
+++ backend/scenario/src/main/java/com/sk/skala/axcalibur/feature/scenario/dto/response/item/ApiItem.java
@@ -1,5 +1,7 @@
 package com.sk.skala.axcalibur.feature.scenario.dto.response.item;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiItem {
+    @JsonProperty("id")
     private String id; // API 아이디
+    @JsonProperty("name")
     private String name; // API 이름
+    @JsonProperty("desc")
     private String desc; // API 설명
 }
