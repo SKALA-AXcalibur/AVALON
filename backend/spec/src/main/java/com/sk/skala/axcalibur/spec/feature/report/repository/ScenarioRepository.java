1b1c790c6d5c22656bba5920653d282230194e48
ard1019 <rio1019@naver.com>
2025-07-06 15:54:31 +0900

    쿼리문 수정 및 시나리오 관련 repository, serviceimpl 오타 수정

--- backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/ScenarioRepository.java
+++ backend/spec/src/main/java/com/sk/skala/axcalibur/spec/feature/report/repository/ScenarioRepository.java
@@ -14,6 +14,6 @@ import com.sk.skala.axcalibur.spec.feature.report.entity.ScenarioEntity;
 public interface ScenarioRepository extends JpaRepository<ScenarioEntity, Integer> {
     Optional<ScenarioEntity> findByScenarioId(String scenarioId);
 
-    List<ScenarioEntity> findByProject_Key(Integer projectKey);
+    List<ScenarioEntity> findByProjectKey_Key(Integer projectKey);
     
 }
