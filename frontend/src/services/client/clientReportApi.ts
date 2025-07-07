0df1fc86efda8433d3d140b1d0d0a1ecbe493286
kimseongyu <kskksk0925@naver.com>
2025-07-07 21:16:02 +0900

    feat: 파일 다운로드 수정

--- frontend/src/services/client/clientReportApi.ts
+++ frontend/src/services/client/clientReportApi.ts
@@ -1,26 +1,24 @@
-import { SuccessResponse, ErrorResponse } from "@/types/api";
-import { handleApiResponse } from "@/utils/apiUtils";
 import ky from "ky-universal";
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/report/v1`;
 
 export const clientReportApi = {
-  readScenarioReport: async (): Promise<Response> => {
+  readScenarioReport: async (): Promise<Blob> => {
     const res = await ky
       .get(`${BASE_URL}/scenario`, {
         credentials: "include",
       })
-      .json<SuccessResponse<Response> | ErrorResponse>();
+      .blob();
 
-    return handleApiResponse(res);
+    return res;
   },
-  readTestcaseReport: async (scenarioId: string): Promise<Response> => {
+  readTestcaseReport: async (scenarioId: string): Promise<Blob> => {
     const res = await ky
       .get(`${BASE_URL}/testcase/${scenarioId}`, {
         credentials: "include",
       })
-      .json<SuccessResponse<Response> | ErrorResponse>();
+      .blob();
 
-    return handleApiResponse(res);
+    return res;
   },
 };
